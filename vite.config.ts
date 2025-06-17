1f79b0bf74e6287a8d08d6a26fd145bf05405eed
Ionita Bogdan <bogdanmariogg11@gmail.com>
2025-06-18 01:52:49 +0300

    Fix vite config and prepare for GitHub Pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,17 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-import topIcon from '../../assets/logo.svg';
-
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/Beatsomnia/", // <-- Add this for GitHub Pages!
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
